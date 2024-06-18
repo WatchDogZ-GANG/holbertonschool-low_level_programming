ea2b30e1e2838c9adbc74f863f1a479b775a684c
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-06-18 14:16:12 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+
+/**
+ *main: Etr√e
+ *puts: Print \"Programming is like building a multilingual puzzle
+ *return: Indique que l'op√ration a r√ussi
+ */
+
+int main()
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
