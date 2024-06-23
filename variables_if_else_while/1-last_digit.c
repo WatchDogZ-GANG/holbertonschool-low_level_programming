2d8a383986dba7238f196bf0cf3d09052e7da6d1
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-06-23 09:20:03 +0000

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,14 +22,16 @@ int main(void)
 	t = n % 10;
 	
 	if (t > 5)
-		
+	{	
 		printf("Last digit of %d is %d and is greater than 5\n"), n, t);
+	}
 	else if (t == 0)
-		
+	{	
 		printf("Last digit of %d is %d and is 0\n");
+	}
 	else
-		
+	{
 		printf("Last digit of %d is %d and is  less than 6 and not 0\n");
-	
+	}
 	return (0);
 }
