8270694f08f645436075fa2214bb3844ba21fac2
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-06-23 14:14:37 +0000

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,7 +3,6 @@
 #include <time.h>
 
 /**
-*
 * main - take last digit and do a verification
 *
 * Return: 0
@@ -12,26 +11,24 @@
 
 int main(void)
 {
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	int t;
-	
-	t = n % 10;
+int n, t;
 
-	if (t > 5)
-	{	
-		printf("Last digit of %d is %d and is greater than 5\n", n, t);
-	}
-	else if (t == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, t);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is  less than 6 and not 0\n", n, t);
-	}
-	
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+t = n % 10;
+
+if (t > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, t);
+}
+else if (t == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, t);
+}
+else if (t < 6)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, t);
+}
+return (0);
 }
