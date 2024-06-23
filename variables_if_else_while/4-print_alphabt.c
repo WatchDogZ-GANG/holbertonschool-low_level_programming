72c06ba7d5168c769224213099efbda1a5d9abad
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-06-23 14:39:57 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#incude <stdio.h>
+
+/**
+* main - Gave alphabet from a to z
+*
+* Return:0
+*/
+
+int main(void)
+{
+
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet != 'q' && alphabet != 'e')
+{
+putchar(alphabet);
+}
+}
+putchar('\n');
+return 0;
+}
