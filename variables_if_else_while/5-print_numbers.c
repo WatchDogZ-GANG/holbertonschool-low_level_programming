5c76604d81a75644cb03d534e85958c65e74c6a2
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-06-23 14:49:14 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Make count 0 to 9
+*
+* Return:0
+*/
+
+int main(void)
+{
+
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+{
+printf("%d", digit);
+}
+return (0);
+}
