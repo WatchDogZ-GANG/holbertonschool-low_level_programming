cb5df5bb8501ba3ead47fb2f363a93b16c3c48d2
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-06-24 13:10:31 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -18,17 +18,15 @@
 
 int main(void)
 {
+	char n[] = "_putchar";
 
-char n[] = "_putchar";
-
-int a = 0;
-{
+	int a = 0;
 
 	for (a = 0; a < 8; a++)
 	{
 		_putchar(n[a]);
 		_putchar('\n');
 	}
-}
-return (0);
+
+	return (0);
 }
