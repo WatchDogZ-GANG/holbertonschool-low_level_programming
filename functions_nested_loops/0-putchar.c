f52f8a374f2eb3e63de8d823700458fad534c29c
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-06-24 12:50:40 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,12 +4,34 @@
 /**
  * main - entry point
  *
+ * char - n[] Les accolades viendrons se servir
+ * de la valeur de a pour afficher le nombre
+ * de lettre en se basant sur a++
+ *
+ * n = _putchar
+ *
+ * for - Pour a = 0, si a inferieur 8, a++ jusque 8
+ * saut de ligne
+ *
  * Return: 0
  */
 
 int main(void)
 {
 
-printf("_putchar\n");
+/*char n &accolade pour definir tableau ASCII& = putchar recall n[]*/
+char n[] = "_putchar";
+
+/*entier a*/
+
+int a = 0;
+{
+
+for (a = 0; a < 8; a++)
+{
+_putchar(n[a]);
+_putchar('\n');
+}
+}
 return (0);
 }
