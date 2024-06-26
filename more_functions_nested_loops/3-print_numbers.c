9a6a5fe4124f7fb05fef8a5cf68a0ca09c0a4787
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-06-26 14:09:25 +0000

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Betty doc
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+
+	int a = 0;
+
+	for (a = 0; a > 10; a++)
+	{
+
+		_putchar('%d', a);
+	}
+
+	_putchar('\n');
+}
