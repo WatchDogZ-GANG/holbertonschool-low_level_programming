6aabc2fe77a2bb3c7e56356f3713e6bbf9ef361e
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-04 15:20:23 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - machin truc
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+{
+
+char a;
+
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar(a);
+}
