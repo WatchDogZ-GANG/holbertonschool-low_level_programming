24594fdb9d65bbcabd484bcd452bfab0787949cf
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-12 12:27:48 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,41 +2,42 @@
 #include "main.h"
 
 /**
- * main - Betty docs
+ * main - Entry point
  *
  * Return: 0
  */
 
 void times_table(void)
 {
+    int a, b, c;
 
-int a, b, c;
+    for (a = 0; a <= 9; a++)
+    {
+        for (b = 0; b <= 9; b++)
+        {
+            c = a * b;
 
-	for (a = 0; a <= 9; a++)
-	{
+            if (b == 0)
+            {
+                _putchar('0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
 
-		_putchar('0');
-		
-		for (b = 1; b <= 9; b++)
-		{
+                if (c <= 9)
+                {
+                    _putchar(' ');
+                }
+                else
+                {
+                    _putchar((c / 10) + '0');
+                }
 
-			_putchar(',');
-			_putchar(' ');
-
-			c = a * b;
-
-			if (b <= 9)
-			{
-
-				_putchar(' ');
-			}
-			else
-			{
-
-				_putchar((c / 10) + '0');
-			}
-		}
-
-		_putchar((c % 10) + '0');
-	}
+                _putchar((c % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
