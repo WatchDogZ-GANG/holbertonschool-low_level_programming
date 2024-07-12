cf3e3bc121bd654acf67948160f71b9a50c8e0c7
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-12 12:10:34 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,30 +12,31 @@ void times_table(void)
 
 int a, b, c;
 
-for (a = 0; a <= 9; a++)
-{
+	for (a = 0; a <= 9; a++)
+	{
 
-_putchar('0');
-for (b = 1; b <= 9; b++)
-{
+		_putchar('0');
+		
+		for (a = 1; a <= 9; a++)
+		{
 
-_putchar(',');
-_putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 
-c = a * b;
+			c = a * b;
 
-if (a <= 9)
-{
+			if (b <= 9)
+			{
 
-_putchar(' ');
-}
-else
-{
+				_putchar(' ');
+			}
+			else
+			{
 
-_putchar((c / 10) + '0');
-}
-}
+				_putchar((c / 10) + '0');
+			}
+		}
 
-_putchar((c % 10) + '0');
-}
+		_putchar((c % 10) + '0');
+	}
 }
