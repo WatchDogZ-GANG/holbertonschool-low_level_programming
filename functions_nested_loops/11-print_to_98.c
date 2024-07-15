f8bb26382ba8ccde5b0c4f678c86dfe387a452fc
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-15 07:56:50 +0000

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+
+
+/**
+ * main - Betty docs
+ *
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+
+int o;
+
+o = 98;
+
+if (n < o, n++)
+	printf("%d, ", n);
+else (n > o, n--)
+	printf("%d, ", n);
+
+}
