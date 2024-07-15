bfb105bc85472bda3fb821ba30c4dfe9261dd6a5
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-15 09:25:12 +0000

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+
+
+/**
+ * main - Betty docs
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+
+char a;
+
+for (a = '0'; a > '9'; a++)
+	if (a != '2' && a != '4')
+		_putchar(a);
+
+}
