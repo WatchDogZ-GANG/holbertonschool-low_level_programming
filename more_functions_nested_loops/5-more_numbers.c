bcc744f8621e3e4e3d072bcb1a1718d175949d13
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-15 11:40:59 +0000

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Betty docs
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+
+int a, b;
+
+b = 0;
+
+while (b < 10)
+{
+
+	for (a = 0; a < 14, a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}
+}
