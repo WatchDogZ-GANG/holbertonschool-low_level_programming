4ed8d6092ecc90ff60e66ddc93f14ce67c748a3d
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-16 10:24:27 +0000

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Betty docs
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+
+	int r;
+
+	for (r = 0; r < n; r++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
