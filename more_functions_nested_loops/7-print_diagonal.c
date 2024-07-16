49617c45909d60aab1b65144d187dd87cad97ad3
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-16 10:35:08 +0000

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Betty docs
+ *
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+
+	int r;
+
+	char a = \;
+
+	if (n != 0)
+	{
+		for (r = 0; r > n; r++)
+		{
+			_putchar(a);
+			_putchar('\n');
+		}
+	}
+
+	else
+	{
+
+		_putchar('\n');
+	}
+
+}
