f7c576bc156a80a6d6a31fae8979431e8b076f39
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-19 13:59:55 +0000

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -12,9 +12,9 @@ void print_triangle(int size)
 
 int a, b; /* Entier pour le nombre de lignes et le nombre de charact√res */
 
-for (a = 1; a <= size; a++) /* Pour a qui est egal 1, si a inferieur ou egal a size, j'incremete a */
+for (a = 0; a <= size; a++) /* Pour a qui est egal 1, si a inferieur ou egal a size, j'incremete a */
 {
-	for (b = 1; b <= size; b++) /* Pareil pour b */
+	for (b = 0; b <= size; b++) /* Pareil pour b */
 	{
 
 		if (b <= size - a) /* Si b est inferieur ou egal a size moins la valuer de a je print l'espace */
