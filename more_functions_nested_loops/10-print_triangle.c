9c2c21c972f6beca6b44c22efe5ac524c0debf0b
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-19 13:19:41 +0000

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Betty docs
+ *
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+
+int a, b;
+
+for (a = 0; a < size; a++)
+{
+	for (b = 0; b < size; b++)
+	{
+	
+		_putchar('#');
+	}
+
+	_putchar('\n');
+}
+}
