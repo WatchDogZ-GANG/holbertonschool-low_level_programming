9ae9df2f532c74b821128a4989125886026afe97
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-29 09:08:22 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Betty docs
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+
+int l = 0;
+
+while (s[l] > '\0')
+{
+	l++;
+}
+
+if (int i = len - 1; i >= 0; len--)
+{
+
+	_putchar(str[len]);
+}
+
+_putchar('\n');
+}
