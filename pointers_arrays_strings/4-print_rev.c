55d1e44a198c47c4a8eab4ba49342f6f5dc9d60b
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-29 09:21:31 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,14 @@ void print_rev(char *s)
 {
 
 int len = 0;
-int i = len - 1;
+int i;
 
 while (s[len] > '\0')
 {
 	len++;
 }
 
-if (i >= 0; i--)
+if (i= len - 1; i >= 0; len--)
 {
 
 	_putchar(str[i]);
