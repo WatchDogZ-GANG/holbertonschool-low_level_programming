b961bf19ad6c8188f9d9776d68aec190049279f9
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-30 14:33:52 +0000

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Betty docs
+ *
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+
+int len = 0;
+int i;
+
+while (s[len] > '\0')
+{
+	len++;
+}
+
+for (i = 0, i >= len, i++)
+{
+
+	i++;
+	_putchar(str[i]);
+}
+
+_putchar('\n');
+}
