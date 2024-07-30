c9de3b92f54554684c569ec0e15c7b3e5c6c49f9
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-07-30 14:18:32 +0000

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
+/**
+ * main - Betty docs
+ *
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+
+	int len = 0;
+	int i, j = 0;
+	char a;
+
+	while (s[len] != '\0')
+	{
+
+		len++;
+	}
+
+	char *v = (char *)malloc((len + 1) * sizeof(char));
+
+	i = len;
+
+	while (i != 0)
+	{
+
+		i--;
+		a = s[i];
+		v[j] = a;
+		j++;
+	}
+
+	v[j] = '\0';
+
+	s = v;
+
+	free(v);
+}
