d20209c2530b2caac4dca7e7a8db00eff8655428
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-08-02 07:30:14 +0000

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Betty docs
+ *
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+
+	int b = 0;
+	int c = 0;
+
+	while (a[b] > n)
+	{
+
+		b++;
+	}
+
+	while (c > b)
+	{
+
+		printf(a);
+		printf(", ");
+		c++;
+	}
+
+	printf('\n');
+
+}
