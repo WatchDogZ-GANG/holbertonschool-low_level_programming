8e0ebe42a18c3fa289d6a727aa0c29ebb3732ae7
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-08-05 08:30:15 +0000

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,23 +10,22 @@
 void print_array(int *a, int n)
 {
 
-	int b = 0;
-	int c = 0;
+	int b;
 
-	while (n[b] > a)
+	for (b = 0; b < n; b++)
 	{
 
-		b++;
-	}
+		if (b != n - 1)
+		{
 
-	while (c > b)
-	{
+			printf("%d", a[b]);
+		}
 
-		putchar(a[c]);
-		printf(", ");
-		c++;
-	}
+		else
+		{
 
-	putchar('\n');
+			printf("%d", a[b];
+		}
 
+		printf("\n");
 }
