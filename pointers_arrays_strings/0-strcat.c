de403bdf82807699b8dcb2b690bb0812fa2372c1
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-08-06 10:06:13 +0000

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+
+	int b = 0;
+
+	while (*(src + b) != '\0')
+	{
+
+		*(dest + b) = *(src + b);
+		b++;
+	}
+	
+	*(dest + b) = '\0';
+	return (dest);
+}
