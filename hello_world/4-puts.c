9e2d449d2d05d0508d39e17c0d9c4f3c6bc2f717
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-21 04:19:54 -0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main: Write "Programming is like building a multilingual puzzle with Puts
  *
- * Return: 0
+ * Return: 1
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (1);
 }
