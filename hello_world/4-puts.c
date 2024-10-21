6abc65f033847184b62fd0c4030ccd7bbc5ea47a
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-21 04:01:45 -0400

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main: Print "Programming is like building a multilingual puzzle with Puts
+ *
+ * Return: O
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
