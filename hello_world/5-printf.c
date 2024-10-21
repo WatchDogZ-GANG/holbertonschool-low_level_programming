4650d23d1d2304283766da8eb67b3bd40933dd70
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-21 04:23:33 -0400

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - "Print with proper grammar, but the outcome is a piece of art," with printf
+ *
+ * Return: O
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
