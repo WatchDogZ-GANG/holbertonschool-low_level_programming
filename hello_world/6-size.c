4210331aa2cdf3d623b6f3647da6cf324c9a3efb
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-21 13:14:34 +0200

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print size of
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char a;
+	int b;
+	long int c
+	long long int d;
+	float e;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of a int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+	printf("Size of a float: %zu byte(s)\n", sizeof(e));
+
+	return (0);
+}
