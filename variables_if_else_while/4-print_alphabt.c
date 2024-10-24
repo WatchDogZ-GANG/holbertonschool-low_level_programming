993e18e7973194acb3cf91eefd0320fcb0dfcdfe
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-24 12:23:04 +0200

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print without Q and E
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char a;
+
+	for (a = '`'; a <= 'z'; a++)
+	{
+		if (a != 'e')
+		{
+			if (a != 'q')
+			{
+				putchar(a);
+			}
+		}
+	}
+	
+	putchar('\n');
+}
