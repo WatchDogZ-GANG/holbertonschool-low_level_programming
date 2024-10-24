66bf0c307e7683a5a006db103be19eb04358e051
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-24 12:28:33 +0200

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print without Q and E
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+	return (0);
+}
