56fec75face82e024015034aeef2402b17783cd7
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-25 02:10:21 +0200

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (a = '9' + 40; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}
