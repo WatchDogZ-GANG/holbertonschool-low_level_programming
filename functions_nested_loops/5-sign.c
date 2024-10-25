35e09abf130abfc0cd1769e6fe1849464d32e1bb
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-25 10:48:52 +0200

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,17 +12,14 @@ int print_sign(int n)
 
 	if (a > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	else if (a < 0)
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
-	else
-	{
-		return (0);
-		_putchar('0');
-	}
+	_putchar('0');
+	return (0);
 }
