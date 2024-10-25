3e0fb21456f8d3a5bce99dec0b6ab6ba8cab5aa2
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-25 09:46:24 +0200

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print Alphabet, one more time
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
