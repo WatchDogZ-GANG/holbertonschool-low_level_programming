f1f815a9eb86a655a9bc39e3ed7fde8615319739
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-28 13:14:31 +0100

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "smain.h"
+
+/**
+ *
+ *
+ *
+ */
+int _isdigit(int c)
+{
+
+	int a = c;
+
+	for (a = c; a >= 0)
+	{
+
+		if (a <= 9)
+		{
+
+			return (1);
+		}
+		else
+		{
+
+			return (0);
+		}
+	}
+	return (0);
+}
