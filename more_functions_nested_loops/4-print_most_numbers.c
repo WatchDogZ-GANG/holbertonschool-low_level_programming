696cd0f416180f0d84f5b427cf834ab5c2ec8e0f
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-29 14:38:29 +0100

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+
+		while (a = '2')
+		{
+
+			a++;
+		}
+
+		while (a = '4')
+		{
+
+			a++;
+		}
+
+		_putchar(a);
+	}
+}
