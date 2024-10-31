6ae72421bb4f7365b50feeb48861ee51f0044ea5
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-10-31 12:11:20 +0100

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - 10 time 0 to 14
+ *
+ * Retrun: 0
+ */
+void more_numbers(void)
+{
+	
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+
+		for (b = 0; b <= 14; b++)
+		{
+
+			if (b > 9)
+			{
+
+				_putchar(b / 10 + 48);
+			}
+
+			_putchar(b % 10 + 48);
+		}
+
+		_putchar('\n');
+	}
+}
