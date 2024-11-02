55dc5dd3d63d5d1611b191e0e7bed9382b95288f
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-11-02 18:51:04 +0100

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - Print n space for n line
+ *
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (a = 0; a < n; a++)
+	{
+
+		b = 0;
+
+		putchar('\');
+		putchar('\n');
+
+		while (b < a)
+		{
+			b++;
+			putchar(' ')
+		}
+	}
+}
