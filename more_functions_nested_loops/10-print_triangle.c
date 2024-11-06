5814d913781a530d75dbcd4cbae8d287203e46e6
WatchDogZ-GANG <103369097+WatchDogZ-GANG@users.noreply.github.com>
2024-11-06 03:18:01 +0100

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_triangle(int size)
+{
+int a;
+int b;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+
+for (a = 1; a <= size; a++)
+{
+for (b = a; b < size; b++)
+_putchar(' ');
+for (b = 1; b <= a; b++)
+_putchar('#');
+_putchar('\n');
+}
+}
