83a40e0f7ed481d68835fdfda469ed8ce19d13e1
WatchDogZ-GANG <103369097+WatchDogZ-GANG@users.noreply.github.com>
2024-11-06 02:36:55 +0100

    8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,2 +1,26 @@
 #include <stdio.h>
+#include "main.h"
 
+/**
+ * print - Print n square for n line
+ *
+ * Return: 0
+ */
+void print_square(int s)
+{
+    int a, b;
+
+    if (s <= 0)
+    {
+        _putchar('\n');
+    }
+
+    for (a = 0; a < s; a++)
+    {
+        for (b = 0; b < s; b++)
+        {
+            _putchar('#');
+        }
+        _putchar('\n');
+    }
+}
