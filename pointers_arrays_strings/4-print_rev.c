eeb31eb85172b343a083a96de190caf96e4fac2d
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-11-23 23:02:13 +0100

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_rev(char *s)
+{
+
+	int a, b;
+
+    for (a = 0; s[a] != '\0'; a++)
+    {
+
+        ;
+    }
+
+	for (b = a; b != 0; b--)
+	{
+
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
