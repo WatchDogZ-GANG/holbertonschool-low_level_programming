6880d594b7bdfb55954873902aa790073a439573
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-11-23 22:10:22 +0100

    3-puts.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,9 +1,8 @@
-#define MAINH
 #ifndef MAINH
+#define MAINH
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-void print_rev(char *s);
 #endif
