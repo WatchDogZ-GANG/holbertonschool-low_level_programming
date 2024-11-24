22f009957692c3dd0af6ce2e4658519815cfb62e
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-11-24 10:53:31 +0100

    5-main.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+void rev_string(char *s)
+{
+
+    int a = 0, b = 0;
+
+    while (s[a])
+    {
+
+        a++;
+    }
+
+    while (--a > b)
+    {
+
+        char c = s[a];
+        s[a] = s[b];
+        s[b] = c;
+        b++;
+    }
+}
