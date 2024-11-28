e3a5abffb157f3ead4889a64c1c4bb5007b89a45
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-11-28 10:59:29 +0100

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+void reverse_array(int *a, int n)
+{
+
+	int b = 0, temp = 0;
+
+	while (s[b])
+	{
+
+		b++;
+	}
+
+	while (b > temp)
+	{
+
+		a = s[b];
+		s[b] = s[temp];
+		s[temp] = a;
+		temp++;
+	}
+}
