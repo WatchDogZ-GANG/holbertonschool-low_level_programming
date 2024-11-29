fd241e884c689b5a183f7ebf8191674a60be945f
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-11-29 12:24:14 +0100

    4array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -3,16 +3,16 @@
 
 void reverse_array(int *a, int n)
 {
-	int j = n / 2; 
-	int i, b = 0;
+	int j = n / 2;
+	int i, k = n, b = 0;
 
 	while (i < j)
 	{
-		b = a[n]; 
-		a[n] = a[i]; 
+		b = a[k]; 
+		a[k] = a[i]; 
 		a[i] = b;
 		i++;
-		n--;
+		k--;
 	}
 
 }
