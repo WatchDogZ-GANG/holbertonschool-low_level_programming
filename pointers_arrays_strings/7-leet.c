6b508e8fba60a8e5677f16a60bb539d8b82eb83b
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-12-02 13:31:49 +0100

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+char *leet(char *a)
+{
+
+    int b;
+
+    for (b = 0; a[b] != '\0'; b++)
+    {
+
+        if (a[b] == 'a' || a[b] == 'A')
+        {
+
+            a[b] = '4';
+        }
+
+        else if (a[b] == 'e' || a[b] == 'E')
+        {
+
+            a[b] = '3';
+        }
+
+        else if (a[b] == 'o' || a[b] == 'O')
+        {
+
+            a[b] = '0';
+        }
+
+        else if (a[b] == 't' || a[b] == 'T')
+        {
+
+            a[b] = '7';
+        }
+
+        else if (a[b] == 'l' || a[b] == 'L')
+        {
+
+            a[b] = '1';
+        }
+
+        else
+        {
+
+            ;
+        }
+    }
+
+    return (a);
+}
