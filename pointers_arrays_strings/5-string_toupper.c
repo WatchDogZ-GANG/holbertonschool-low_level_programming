a557c1b96d2056a82a32a505441969197835e8a0
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-12-02 09:49:50 +0100

    5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -4,15 +4,15 @@
 char *string_toupper(char *a)
 {
 
-    int i;
+	int i;
 
-    while (i = 0; a[i] != '\0'; i++)
-    {
-        if (a[i] >= 'a' && a[i] <= 'z')
-        {
-            a = a - 32;
-        }
-    }
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		while (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a = a - 32;
+		}
+	}
 
-    return (a);
+	return (a);
 }
