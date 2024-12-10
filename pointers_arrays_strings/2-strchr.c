4e6d63bfbf82744557c31070ed330161d1ae8871
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-12-10 10:30:47 +0100

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
 char *_strchr(char *s, char c)
 {
 
-    while (*s <= '\0')
+    while (*s != '\0')
     {
 
         if (*s == c)
@@ -15,5 +15,5 @@ char *_strchr(char *s, char c)
         s++;
     }
 
-    return ('\0');
+    return (c);
 }
