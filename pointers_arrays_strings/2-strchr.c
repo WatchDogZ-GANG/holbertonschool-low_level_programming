9bbe04914425ebd819a399f39e937002e6bdf68b
WatchDogZ-GANG <watchdogzgang@gmail.com>
2024-12-10 10:44:49 +0100

    Je ne savais pas si on pouvait se servir de librairies et pensait NULL comme étant présent de base 🤣👌

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,23 +2,14 @@
 
 char *_strchr(char *s, char c)
 {
+	int i;
 
-    while (*s <= '\0')
-    {
-
-        if (*s == c)
-        {
-
-            return (s);
-        }
-        if (*s == '\0')
-        {
-
-            return (s);
-        }
-
-        s++;
-    }
-
-    return (s);
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
 }
